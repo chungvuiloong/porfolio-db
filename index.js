1ee30ecc4b90ac1d159cb2442737f1357b934deb
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2024-03-21 10:27:55 +0200

    Updated backend

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = process.env.SERVER_PORT || 3001;
 
 const app = express();
 
-// app.use("/", (req, res) => {
-//   res.send("server running");
-// });
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`)
+//   })
+
+app.use(express.static('build'))
+app.use(cors())
+app.use(express.json())
+
 app.get("/", (req, res) => {
-  res.json("server running");
+//   res.json("server running");
+  console.log("Running server");
 });
 
 app.listen(PORT, () => {
